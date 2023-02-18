1411bc860a776ec7fb73de1aea59ad60eec187f8
Renzo Viscio <nviscio@gmail.com>
2023-02-17 23:54:38 -0300

    About

--- src/components/Form/validation.js
+++ src/components/Form/validation.js
@@ -5,19 +5,19 @@ export function validate(objUser) {
   const errors = {};
 
   if (!regexEmail.test(objUser.username)) {
-    errors.username = "El usuario debe ser un email";
+    errors.username = "*El usuario debe ser un email";
   } else if (!objUser.username) {
-    errors.username = "El nombre de usuario no puede estar vacío";
+    errors.username = "*El nombre de usuario no puede estar vacío";
   } else if (objUser.username.length > 35) {
     errors.username =
-      "El nombre de usuario no puede tener más de 35 caracteres";
+      "*El nombre de usuario no puede tener más de 35 caracteres";
   }
 
   if (!regexPassword.test(objUser.password)) {
-    errors.password = "La contraseña debe tener al menos un número";
+    errors.password = "*La contraseña debe tener al menos un número";
   } else if (objUser.password.length < 6 || objUser.password.length > 10) {
     errors.password =
-      "La contraseña debe tener una longitud entre 6 y 10 caracteres";
+      "*La contraseña debe tener una longitud entre 6 y 10 caracteres";
   }
   return errors;
 }
--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -10,11 +10,11 @@ export default function Nav(props) {
       <Link to="/home">
         <span>Home</span>
       </Link>
-      <Link to="/about">
-        <span>About</span>
-      </Link>
       <Link to="/favorites">
         <span>Favorites</span>
+      </Link>
+      <Link to="/about">
+        <span>About</span>
       </Link>
       <SearchBar onSearch={props.onSearch} />
     </div>
